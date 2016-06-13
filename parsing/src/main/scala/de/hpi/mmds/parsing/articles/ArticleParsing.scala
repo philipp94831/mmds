fee40d1e8e07744414645c0b2602e52866a428bd
mariannet <marianne.thieffry@t-online.de>
2016-06-13 02:33:16 +0200

    Finally got parsing and outputting with one line per document to work, input and output path are arguments.

--- parsing/src/main/scala/de/hpi/mmds/parsing/articles/ArticleParsing.scala
+++ parsing/src/main/scala/de/hpi/mmds/parsing/articles/ArticleParsing.scala
@@ -1,71 +1,60 @@
 package de.hpi.mmds.sparking.articles
 
-import com.databricks.spark.xml.XmlInputFormat
+// import spark stuff
 import org.apache.spark.{SparkConf, SparkContext}
+
+// import xml reading stuff
+import com.databricks.spark.xml.XmlInputFormat
 import org.apache.hadoop.io.{LongWritable, Text}
 
-class ArticleParser(input: String) {
+// import xml transforming stuff
+import scala.xml.XML
+
+class ArticleParser(input: String, output: String) {
   val sc = {
     // If we use spark-submit, the SparkContext will be configured for us.
     val conf = new SparkConf(true)
     conf.setIfMissing("spark.master", "local[2]") // Run locally by default.
-    conf.setAppName(s"lda ($input)")
+    conf.setAppName(s"lda ($input) ($output)")
     new SparkContext(conf)
   }
   
   def run() = {
+    // DEBUG
+    sc.setLogLevel("ERROR")
+    
     // configure hadoop
     sc.hadoopConfiguration.set(XmlInputFormat.START_TAG_KEY, "<page>")
     sc.hadoopConfiguration.set(XmlInputFormat.END_TAG_KEY, "</page>")
     sc.hadoopConfiguration.set(XmlInputFormat.ENCODING_KEY, "utf-8")
     
-    val path = "C:/Users/Marianne/Documents/Uni/HPI/Semester_1/MMDS-Mining_Massive_Datasets/data/enwiki-20160501-pages-articles1-20articles.xml-p000000010p000030302"
-    
     // read file
-    val records = sc.newAPIHadoopFile(path, classOf[XmlInputFormat], classOf[LongWritable], classOf[Text])
+    val rdd_input = sc.newAPIHadoopFile(input, classOf[XmlInputFormat], classOf[LongWritable], classOf[Text])
+    val rdd_text = rdd_input.map {
+      case (writable, text) => (writable.toString, text.toString)
+    }
 
+    val rdd_single_line = rdd_text
+        .map{ s =>
+            val xml = XML.loadString(s._2)
+            val id = (xml \ "id").text.toInt
+            val title = (xml \ "title").text
+            val text = (xml \ "revision" \ "text").text.replaceAll("\\W", " ")
+            val tknzed = text.split("\\W").filter(_.size > 3).toList
+          (id, title, tknzed )
+        }
     
-    
-    
-    
-    
-    
-    
-    // DEBUG
-    sc.setLogLevel("ERROR")
-    records.foreach(println)
-    
-    println("\n")
-    println("\n")
-    println("\n")
-    println("\n")
-    println("\n")
-    println("\n")
-    println("\n")
-    println("\n")
-    println("\n")
-    println("\n")
-    println("\n")
-    println("\n")
-    println("\n")
-    println("\n")
-    println("Number of records: ")
-    println(records.count())
-    println("\n")
+    rdd_single_line.saveAsTextFile(output)
   }
 }
 
 object ArticleParser {
   def main(args: Array[String]): Unit = {
     if (args.isEmpty) {
-      println("Usage: scala <main class> <input file location>")
+      println("Usage: scala <main class> <input file location> <output file location>")
       sys.exit(1)
     }
-
-    val startTime = java.lang.System.currentTimeMillis
-    new ArticleParser(args(0)).run()
-    val endTime = java.lang.System.currentTimeMillis
-
-    println(f"Finished in ${endTime - startTime}%,d ms.")  }
+    new ArticleParser(args(0), args(1)).run()
+  }
 
 }
