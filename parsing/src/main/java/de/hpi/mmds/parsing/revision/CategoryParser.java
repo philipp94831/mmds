b0180f09d27cb90525214634c8d8c9583d1c5cd1
Richard Trebichavský <richard.trebichavsky@gmail.com>
2016-06-15 01:15:24 +0200

    Implemented job that combines 'category' and 'categorylinks'

--- parsing/src/main/java/de/hpi/mmds/parsing/revision/CategoryParser.java
+++ parsing/src/main/java/de/hpi/mmds/parsing/revision/CategoryParser.java
@@ -1,14 +1,15 @@
 package de.hpi.mmds.parsing.revision;
 
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
 import org.apache.commons.io.FileUtils;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import de.hpi.mmds.wiki.SparkUtil;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class CategoryParser {
 
@@ -20,8 +21,56 @@ public class CategoryParser {
 	private static final int STATE_IN_STRING = 3;
 	private static final int STATE_IN_STRING_ESCAPED = 4;
 
-	public static void main(String[] args) {
-		parseCategories();
+	public static void main(String[] args)
+	{
+		// First run
+//		parseCategories();
+//		parseCategoryLinks();
+
+		// Second run
+		canonicalizateCategories();
+	}
+
+	private static void canonicalizateCategories() {
+
+		HashMap<String, Integer> categories = new HashMap<>();
+		CSVReader reader;
+
+		try {
+			reader = new CSVReader(new FileReader(OUTPUT_DIR + "category.txt/part-00000"));
+			String[] line;
+			while ((line = reader.readNext()) != null) {
+				categories.put(line[1], Integer.parseInt(line[0]));
+            }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		try (JavaSparkContext sc = SparkUtil.getContext()) {
+			JavaRDD<String> categoryLinks = sc.textFile(OUTPUT_DIR + "categorylinks.txt/part-00000");
+			JavaRDD<String> resolved = categoryLinks.map(line -> {
+				String[] entry = csvToEntry(line);
+				assert entry != null;
+
+				Integer categoryId = categories.get(entry[1]);
+				if (categoryId == null) {
+					//System.err.println("Id of category '" + entry[1] + "' is unknown, skipping.");
+					return ""; // TODO: filter empty liens afterwards
+				}
+
+				if (entry.length < 7) {
+					System.err.println("OUT OF BOUNDS: " + Arrays.toString(entry));
+					return "";
+				}
+
+				return entryToCsv(Arrays.asList(entry[0], categoryId.toString(), entry[6]));
+			});
+
+			writeOutput(resolved, OUTPUT_DIR + "resolved.txt");
+
+		} catch (Exception e) {
+			// nothing to do
+		}
 	}
 
 
@@ -30,7 +79,7 @@ public class CategoryParser {
 		try (JavaSparkContext sc = SparkUtil.getContext()) {
 			JavaRDD<String> textFile = sc.textFile(INPUT_DIR + "enwiki-20160407-category.sql");
 			JavaRDD<String> filteredLines = textFile.filter(line -> line.startsWith("INSERT INTO"));
-			JavaRDD<String> entries = filteredLines.flatMap(CategoryParser::parseLine);
+			JavaRDD<String> entries = filteredLines.flatMap(CategoryParser::parseSQLLineToCSV);
 
 			writeOutput(entries, OUTPUT_DIR + "category.txt");
 
@@ -39,10 +88,24 @@ public class CategoryParser {
 		}
 	}
 
+	private static void parseCategoryLinks()
+	{
+		try (JavaSparkContext sc = SparkUtil.getContext()) {
+			JavaRDD<String> textFile = sc.textFile(INPUT_DIR + "enwiki-20160407-categorylinks.sql");
+			JavaRDD<String> filteredLines = textFile.filter(line -> line.startsWith("INSERT INTO"));
+			JavaRDD<String> entries = filteredLines.flatMap(CategoryParser::parseSQLLineToCSV);
 
-	public static List<String> parseLine(String line) {
-		List entries = new ArrayList();
-		ArrayList parts = new ArrayList();
+			writeOutput(entries, OUTPUT_DIR + "categorylinks.txt");
+
+		} catch (Exception e) {
+			// nothing to do
+		}
+	}
+
+
+	public static List<String> parseSQLLineToCSV(String line) {
+		List<List<String>> entries = new ArrayList<>();
+		List<String> parts = new ArrayList<>();
 		String entryBuffer = "";
 		int currentState = STATE_OUTSIDE;
 
@@ -71,7 +134,7 @@ public class CategoryParser {
 							parts.add(entryBuffer);
 							entryBuffer = "";
 							entries.add(parts);
-							parts = new ArrayList();
+							parts = new ArrayList<>();
 							currentState = STATE_OUTSIDE;
 							break;
 						default:
@@ -99,7 +162,26 @@ public class CategoryParser {
 			}
 		}
 
-		return entries;
+		return entries.stream().map(CategoryParser::entryToCsv).collect(Collectors.toList());
+	}
+
+
+	public static String entryToCsv(List<String> entry) {
+		return entryToCsv(entry, '"');
+	}
+
+
+	public static String entryToCsv(List<String> entry, char qm) {
+		StringWriter outCSV = new StringWriter();
+		try {
+			CSVWriter writer = new CSVWriter(outCSV, ',', qm, "");
+			writer.writeNext(entry.toArray(new String[]{}));
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return outCSV.toString();
 	}
 
 
@@ -112,4 +194,14 @@ public class CategoryParser {
 		data.coalesce(1).saveAsTextFile(outFile);
 	}
 
+	public static String[] csvToEntry(String csvLine) {
+		CSVReader reader = new CSVReader(new StringReader(csvLine));
+		try {
+			return reader.readNext();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
 }
--- parsing/src/test/java/de/hpi/mmds/wiki/parsing/CategoryParserTest.java
+++ parsing/src/test/java/de/hpi/mmds/wiki/parsing/CategoryParserTest.java
@@ -2,27 +2,61 @@ package de.hpi.mmds.wiki.parsing;
 
 import de.hpi.mmds.parsing.revision.CategoryParser;
 import org.junit.Assert;
-import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
+
+import org.junit.Test;
 
 public class CategoryParserTest {
 
 	@Test
-	public void testParseSimpleLine() {
+	public void testParseSQLLineToCSVSimple() {
 		String line = "INSERT INTO (1,2,3),(a,b,c)\n";
-		List entries = CategoryParser.parseLine(line);
-
-		Assert.assertEquals(entries.toString(), "[[1, 2, 3], [a, b, c]]");
+		List entries = CategoryParser.parseSQLLineToCSV(line);
+		Assert.assertEquals(Arrays.asList("\"1\",\"2\",\"3\"", "\"a\",\"b\",\"c\""), entries);
 	}
 
 	@Test
-	public void testParseEscapedLine() {
+	public void testParseSQLLineToCSVEscaped() {
 		String line = "INSERT INTO ('\n(\\'a\\')\n',b,c),(d,e,f)\n";
+		List entries = CategoryParser.parseSQLLineToCSV(line);
+		Assert.assertEquals(
+			Arrays.asList(
+				"\"\n('a')\n\",\"b\",\"c\"",
+				"\"d\",\"e\",\"f\""
+			),
+			entries
+		);
+	}
 
-		List entries = CategoryParser.parseLine(line);
-		System.out.println(entries.toString());
+//	@Test
+//	public void testParseArrayLikeLineSimple() {
+//		String line = "[a, b, c]\n";
+//		List entries = CategoryParser.parseArrayLikeLine(line);
+//		Assert.assertEquals("[[a, b, c]]", entries.toString());
+//	}
+//
+//	@Test
+//	public void testParseArrayLikeLineEscaped() {
+//		String line = "[a, b\\,, c']\n";
+//		List entries = CategoryParser.parseArrayLikeLine(line);
+//		Assert.assertEquals("[[a, b,, c']]", entries.toString());
+//	}
 
-		Assert.assertEquals(entries.toString(), "[[\n('a')\n, b, c], [d, e, f]]");
+	@Test
+	public void testEntryToCsv() {
+		String[] entry = {"a,", "b,\",", "c'"};
+		Assert.assertEquals("\"a,\",\"b,\"\",\",\"c'\"", CategoryParser.entryToCsv(Arrays.asList(entry)));
+	}
+
+	@Test
+	public void testCsvToEntry()
+	{
+		String csv = "\"a,\",\"b,\"\",\",c'\r\n";
+		Assert.assertArrayEquals(
+			new String[]{"a,", "b,\",", "c'"},
+			CategoryParser.csvToEntry(csv)
+		);
 	}
 }
