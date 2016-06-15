3e6a74edeb9ee86003d68bdb5036e445e3662b18
mariannet <marianne.thieffry@t-online.de>
2016-06-16 01:03:54 +0200

    Less intermediate values saved, added features: remove stopwords and don't save raw text but word-occurance vector

--- parsing/src/main/scala/de/hpi/mmds/parsing/articles/ArticleParsing.scala
+++ parsing/src/main/scala/de/hpi/mmds/parsing/articles/ArticleParsing.scala
@@ -3,12 +3,17 @@ package de.hpi.mmds.sparking.articles
 // import spark stuff
 import org.apache.spark.{SparkConf, SparkContext}
 
-// import xml reading stuff
+// import xml stuff
 import com.databricks.spark.xml.XmlInputFormat
 import org.apache.hadoop.io.{LongWritable, Text}
-
-// import xml transforming stuff
 import scala.xml.XML
+
+// import text transformation stuff
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.feature.{CountVectorizer, CountVectorizerModel, RegexTokenizer, StopWordsRemover}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.Row
 
 class ArticleParser(input: String, output: String) {
   val sc = {
@@ -23,37 +28,109 @@ class ArticleParser(input: String, output: String) {
     // DEBUG
     sc.setLogLevel("ERROR")
     
+    val rdd_input = read_files(sc, input)
+    //val (rdd_stopwords, vocabArray) = remove_stopwords(rdd_input, 10000)
+    
+    val sqlContext= new org.apache.spark.sql.SQLContext(sc)
+      import sqlContext.implicits._
+    
+    val df = rdd_input.toDF("id", "title", "text")
+    val tokenizer = new RegexTokenizer()
+        .setInputCol("text")
+        .setOutputCol("rawTokens")
+    val stopWordsRemover = new StopWordsRemover()
+        .setInputCol("rawTokens")
+        .setOutputCol("tokens")
+    val countVectorizer = new CountVectorizer()
+        .setVocabSize(10000)
+        .setInputCol("tokens")
+        .setOutputCol("features")
+    val pipeline = new Pipeline()
+        .setStages(Array(tokenizer, stopWordsRemover, countVectorizer))
+    val model = pipeline.fit(df)
+    
+    val rdd_stopwords = model
+        .transform(df)
+        .select("id", "title", "features")
+        .rdd
+        .map({
+            case Row(id: Int, title: String, text)
+            => (id, title, text)
+        })
+    
+    val rdd_output = rdd_stopwords.map({
+        s => (s._1.toString + ';' + s._2 + ';' + s._3)
+    })
+    
+    rdd_output.saveAsTextFile(output)
+  }
+  
+  private def read_files(
+      sc: SparkContext,
+      path: String)
+      : (RDD[(Int, String, String)]) = {  
     // configure hadoop
     sc.hadoopConfiguration.set(XmlInputFormat.START_TAG_KEY, "<page>")
     sc.hadoopConfiguration.set(XmlInputFormat.END_TAG_KEY, "</page>")
     sc.hadoopConfiguration.set(XmlInputFormat.ENCODING_KEY, "utf-8")
     
     // read file
-    val rdd_input = sc.newAPIHadoopFile(input, classOf[XmlInputFormat], classOf[LongWritable], classOf[Text])
+    val rdd_input = sc.newAPIHadoopFile(path, classOf[XmlInputFormat], classOf[LongWritable], classOf[Text])
 
-    val rdd_xml_parsed = rdd_input.map{ s =>
-        val xml = XML.loadString(s._2.toString)
-        val id = (xml \ "id").text.toInt
-        val title = (xml \ "title").text
-        val text = (xml \ "revision" \ "text").text
-      (id, title, text )
-    }
-    val rdd_no_redirect = rdd_xml_parsed.filter(s => !s._3.startsWith("#REDIRECT"))
-    val rdd_no_special_characters = rdd_no_redirect.map{ s =>
-        val replaced = s._3.replaceAll("\\W", " ")
-      (s._1, s._2, replaced)
-    }
-    val rdd_csv = rdd_no_special_characters.map{ s =>
-        val text = s._1.toString + ',' + s._2 + ',' + s._3
-      (text)
-    }
-    
-    rdd_csv.saveAsTextFile(output)
+    // transform rdd to usable format
+    val rdd_preprocessing = rdd_input
+      .map({ s =>
+          val xml = XML.loadString(s._2.toString)
+          val id = (xml \ "id").text.toInt
+          val title = (xml \ "title").text
+          val text = (xml \ "revision" \ "text").text
+        (id, title, text )
+      })
+      .filter(s => !s._3.startsWith("#REDIRECT"))
+      .map({ s =>
+          val replaced = s._3.replaceAll("\\W", " ")
+        (s._1, s._2, replaced)
+      })
+    (rdd_preprocessing)
   }
+  
+  //private def remove_stopwords(
+  //    rdd_before : RDD[(Int, String, String)],
+  //    vocabSize : Int)
+  //    : (RDD[(Int, String, Vector[String])], Array[String]) = {
+  //  val sqlContext= new org.apache.spark.sql.SQLContext(sc)
+  //    import sqlContext.implicits._
+  //  
+  //  val df = rdd_before.toDF("id", "title", "text")
+  //  val tokenizer = new RegexTokenizer()
+  //      .setInputCol("text")
+  //      .setOutputCol("rawTokens")
+  //  val stopWordsRemover = new StopWordsRemover()
+  //      .setInputCol("rawTokens")
+  //      .setOutputCol("tokens")
+  //  val countVectorizer = new CountVectorizer()
+  //      .setVocabSize(vocabSize)
+  //      .setInputCol("tokens")
+  //      .setOutputCol("features")
+  //  val pipeline = new Pipeline()
+  //      .setStages(Array(tokenizer, stopWordsRemover, countVectorizer))
+  //  val model = pipeline.fit(df)
+  //  
+  //  val rdd_after = model
+  //      .transform(df)
+  //      .select("id", "title", "features")
+  //      .rdd
+  //      .map({
+  //          case Row(id: Int, title: String, text: Vector[String])
+  //          => (id, title, text)
+  //      })
+  //  
+  //  (rdd_after, model.stages(2).asInstanceOf[CountVectorizerModel].vocabulary)
+  //}
 }
 
 object ArticleParser {
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {    
     if (args.isEmpty) {
       println("Usage: scala <main class> <input file location> <output file directory, nonexistent>")
       sys.exit(1)
