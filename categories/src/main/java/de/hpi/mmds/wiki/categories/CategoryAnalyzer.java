cceb8e35a7431bd0769ca9a93cf792301a946d84
Richard Trebichavský <richard.trebichavsky@gmail.com>
2016-06-19 15:10:19 +0200

    CategoryAnalyzer: Added comments

--- categories/src/main/java/de/hpi/mmds/wiki/categories/CategoryAnalyzer.java
+++ categories/src/main/java/de/hpi/mmds/wiki/categories/CategoryAnalyzer.java
@@ -12,7 +12,10 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 import scala.Tuple2;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -38,21 +41,34 @@ public class CategoryAnalyzer implements Recommender {
 	}
 
 	public static CategoryAnalyzer build(JavaSparkContext jsc, String treeData, String articleData) {
+		// Loads 'subcat' categoryLinks into tuples: (parent_category_id, subcategory_id)
 		JavaRDD<Tuple2<Integer, Integer>> edges = jsc.textFile(treeData).map(CategoryAnalyzer::parseParentCategories);
+
+		// Performs transitive closure, e.g. if there are tuples (1,2) and (2,3), it will add a new tuple (1,3) to list
 		JavaPairRDD<Integer, Integer> transitiveCategories = TransitiveClosure.compute(edges)
 				.flatMap(t -> Arrays.asList(t, new Tuple2<>(t._1, t._1))).mapToPair(t -> t);
+
+		// Loads 'page' categoryLinks into tuples: (category_id, article_id) and cashes them
 		JavaPairRDD<Integer, Integer> articleCategories = jsc.textFile(articleData)
 				.mapToPair(CategoryAnalyzer::parseArticleCategories).cache();
 
-		// (category, article)
+		// Produces pairs (category, article), which means that article is in category
 		JavaPairRDD<Integer, Integer> transitiveCategoriesForArticles = articleCategories.join(transitiveCategories)
 				.mapToPair(t -> t._2).cache();
+
 		long articleCount = articleCategories.values().count();
-		JavaPairRDD<Integer, Double> entropies = transitiveCategoriesForArticles.mapValues(i -> 1)
+
+		// Computes information gain for each category, given by formula G = 1 + x*log(2,x) + y*log(2,y))
+		//   where 'x' is number of articles in given category and 'y' the overall number of articles
+		// TODO: use actual I.G. formula instead of simple average
+		JavaPairRDD<Integer, Double> informationGains = transitiveCategoriesForArticles.mapValues(i -> 1)
 				.reduceByKey(Integer::sum).filter(t -> t._2 > 0).mapValues(i -> 1.0 - (double) i / articleCount);
 
+		// Creates tuple (article_id, ((category_id, category_information_gain), ...)) for each article, where category
+		//   vector will contain information gain of category, if article is in category, or 0 otherwise
 		JavaPairRDD<Integer, Iterable<Tuple2<Integer, Double>>> categoriesPerArticle = transitiveCategoriesForArticles
-				.join(entropies).mapToPair(t -> new Tuple2<>(t._2._1, new Tuple2<>(t._1, t._2._2))).groupByKey();
+				.join(informationGains).mapToPair(t -> new Tuple2<>(t._2._1, new Tuple2<>(t._1, t._2._2))).groupByKey();
+
 		return new CategoryAnalyzer(setSimilarityJoin(categoriesPerArticle));
 	}
 
@@ -66,8 +82,9 @@ public class CategoryAnalyzer implements Recommender {
 	}
 
 	private static Tuple2<Integer, Integer> parseParentCategories(String v1) {
+		// parent,child,#artices,#subcats
 		String[] split = v1.split(",");
-		if (split.length != 2) {
+		if (split.length < 2) {
 			throw new IllegalStateException("Data malformed");
 		}
 		return new Tuple2<>(Integer.parseInt(split[0]), Integer.parseInt(split[1]));
