11821994cb349b70409b4dea239eb1c2c487aafa
mariannet <marianne.thieffry@t-online.de>
2016-06-19 03:26:46 +0200

    Forgot file in previous commit

---
+++ lda/src/main/java/de/hpi/mmds/wiki/lda/LDA_Recommender.java
@@ -0,0 +1,18 @@
+package de.hpi.mmds.wiki.lda;
+
+import de.hpi.mmds.wiki.Recommendation;
+import de.hpi.mmds.wiki.Recommender;
+
+import org.apache.spark.api.java.JavaRDD;
+
+import java.util.List;
+
+public class LDA_Recommender implements Recommender {
+
+	@Override
+	public List<Recommendation> recommend(int userId, JavaRDD<Integer> articles, int howMany) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
