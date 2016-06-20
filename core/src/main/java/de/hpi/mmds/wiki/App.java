2adc406c7888a215bcf813488994c95dda2ca862
Philipp Schirmer <philipp@schirmer-online.de>
2016-06-20 14:41:52 +0200

    fix bugs

--- core/src/main/java/de/hpi/mmds/wiki/App.java
+++ core/src/main/java/de/hpi/mmds/wiki/App.java
@@ -2,7 +2,7 @@ package de.hpi.mmds.wiki;
 
 import de.hpi.mmds.wiki.categories.CategoryAnalyzer;
 import de.hpi.mmds.wiki.cf.CollaborativeFiltering;
-import de.hpi.mmds.wiki.lda.LDA;
+import de.hpi.mmds.wiki.lda.LDA_Recommender;
 
 import org.apache.spark.api.java.JavaSparkContext;
 
@@ -13,6 +13,7 @@ public class App {
 
 	private static final String CF_DIR = null;
 	private static final String CAT_DIR = null;
+	private static final String LDA_DIR = null;
 	private static final String TEST_DIR = null;
 	private static final String TRAINING_DIR = null;
 	private static final String OUT_FILE = null;
@@ -22,7 +23,7 @@ public class App {
 			Edits test = new Edits(jsc, TEST_DIR);
 			Edits training = new Edits(jsc, TRAINING_DIR);
 			MultiRecommender recommender = new MultiRecommender().add(CollaborativeFiltering.load(jsc, CF_DIR, fs))
-					.add(CategoryAnalyzer.load(jsc, CAT_DIR)).add(new LDA());
+					.add(CategoryAnalyzer.load(jsc, CAT_DIR)).add(LDA_Recommender.load(jsc.sc(), LDA_DIR));
 			Evaluator eval = new Evaluator(recommender, test, training, new File(OUT_FILE));
 			eval.evaluate(200, 100);
 		} catch (IOException e) {
--- lda/src/main/java/de/hpi/mmds/wiki/lda/LDA_Recommender.java
+++ lda/src/main/java/de/hpi/mmds/wiki/lda/LDA_Recommender.java
@@ -1,49 +1,49 @@
 package de.hpi.mmds.wiki.lda;
 
 // import our stuff
+
 import de.hpi.mmds.wiki.Recommendation;
 import de.hpi.mmds.wiki.Recommender;
 
-// import spark stuff
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.SparkContext;
-import org.apache.spark.api.java.function.Function;
-
-// import java stuff
-import java.util.ArrayList;
-import java.util.List;
-import java.io.Serializable;
-
-// import LDA stuff
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.mllib.clustering.DistributedLDAModel;
 
+import scala.Tuple2;
+import scala.Tuple3;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LDA_Recommender implements Serializable, Recommender {
-  
-  private final DistributedLDAModel model;
-  private static final long serialVersionUID = 5290472017062948753L;
+
+	private final transient DistributedLDAModel model;
+	private static final long serialVersionUID = 5290472017062948753L;
 
 	@Override
 	public List<Recommendation> recommend(int userId, JavaRDD<Integer> articles, int howMany) {
 		return recommend(articles.first(), howMany);
 	}
-  
-  public List<Recommendation> recommend(Integer article, int howMany) {
-    JavaRDD<scala.Tuple3<Long, int[], double[]>> topics = model.javaTopTopicsPerDocument(1).filter(new Function<scala.Tuple3<Long, int[], double[]>, Boolean>() { private static final long serialVersionUID = 5290472017062948754L; public Boolean call(scala.Tuple3<Long, int[], double[]> tuple) {return tuple._1().equals(article);}});
-    int topic = topics.first()._2()[0];
-    scala.Tuple2<long[],double[]> articles = model.topDocumentsPerTopic(howMany)[topic];
-    List<Recommendation> recommendations = new ArrayList<Recommendation>();
-    for (int i = 0; i < articles._1().length; i++) {
-      recommendations.add(new Recommendation(articles._2()[i], (int)articles._1()[i]));
-      System.out.println("Article: " + articles._1()[i] + " Probability: " + articles._2()[i]);
-    }
-    return recommendations;
-  }
 
-  public static LDA_Recommender load(SparkContext sc, String path) {
+	public List<Recommendation> recommend(long article, int howMany) {
+		JavaRDD<Tuple3<Long, int[], double[]>> topics = model.javaTopTopicsPerDocument(1)
+				.filter(tuple -> tuple._1() == article);
+		int topic = topics.first()._2()[0];
+		Tuple2<long[], double[]> articles = model.topDocumentsPerTopic(howMany)[topic];
+		List<Recommendation> recommendations = new ArrayList<Recommendation>();
+		for (int i = 0; i < articles._1().length; i++) {
+			recommendations.add(new Recommendation(articles._2()[i], (int) articles._1()[i]));
+			System.out.println("Article: " + articles._1()[i] + " Probability: " + articles._2()[i]);
+		}
+		return recommendations;
+	}
+
+	public static LDA_Recommender load(SparkContext sc, String path) {
 		return new LDA_Recommender(DistributedLDAModel.load(sc, path));
 	}
-  
-  public LDA_Recommender(DistributedLDAModel model) {
-    this.model = model;
-  }
+
+	public LDA_Recommender(DistributedLDAModel model) {
+		this.model = model;
+	}
 }
